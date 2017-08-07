0f7ef6b393095911335f212c361e66e61c8f3ca6
Adam Pogwizd <adampog77@gmail.com>
2017-08-07 18:51:14 -0500

    Small default config change

--- config/config.go
+++ config/config.go
@@ -1,10 +1,10 @@
 package config
 
 const PopSize = 200
-const NumCities = 10
+const NumCities = 11
 
 const TournamentK = 20
 const GenLimit = 500
 
-const CrossoverRate = 0.6
+const CrossoverRate = 0.7
 const MutationRate = 0.03
